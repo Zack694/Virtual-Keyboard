136d72c50764c0ff6fe18e53aa409cdaf7677767
BobScottz <167954955+Zack694@users.noreply.github.com>
2026-02-08 18:10:13 +0800

    Update ChatScreenMixin.java

--- src/main/java/com/commandgui/mixin/ChatScreenMixin.java
+++ src/main/java/com/commandgui/mixin/ChatScreenMixin.java
@@ -6,7 +6,6 @@ import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ChatScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.TextFieldWidget;
-import net.minecraft.client.input.Click;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -17,25 +16,25 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ChatScreen.class)
 public abstract class ChatScreenMixin extends Screen {
-    
+
     @Shadow
     protected TextFieldWidget chatField;
-    
+
     @Unique
     private VirtualKeyboard virtualKeyboard;
-    
+
     protected ChatScreenMixin() {
         super(null);
     }
-    
+
     @Inject(method = "init", at = @At("TAIL"))
     private void onInit(CallbackInfo ci) {
         ChatScreen screen = (ChatScreen) (Object) this;
-        
+
         // Initialize keyboard
         int keyboardX = (screen.width - 325) / 2;
         int keyboardY = screen.height - 135;
-        
+
         virtualKeyboard = new VirtualKeyboard(keyboardX, keyboardY, new VirtualKeyboard.KeyboardListener() {
             @Override
             public void onKeyTyped(String key) {
@@ -43,7 +42,7 @@ public abstract class ChatScreenMixin extends Screen {
                     chatField.write(key);
                 }
             }
-            
+
             @Override
             public void onBackspace() {
                 if (chatField != null && !chatField.getText().isEmpty()) {
@@ -56,7 +55,7 @@ public abstract class ChatScreenMixin extends Screen {
                     }
                 }
             }
-            
+
             @Override
             public void onEnter() {
                 if (chatField != null) {
@@ -76,14 +75,14 @@ public abstract class ChatScreenMixin extends Screen {
             }
         }, screen);
     }
-    
+
     @Inject(method = "render", at = @At("HEAD"))
     private void onRenderStart(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
         if (virtualKeyboard != null) {
             virtualKeyboard.tick();
         }
     }
-    
+
     @Inject(method = "render", at = @At("TAIL"))
     private void onRender(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
         if (virtualKeyboard != null) {
@@ -91,37 +90,34 @@ public abstract class ChatScreenMixin extends Screen {
             virtualKeyboard.renderButtons(context, mouseX, mouseY, delta);
         }
     }
-    
-    // FIXED: Mixin intercept now uses Click record (1.21.11)
+
     @Inject(method = "mouseClicked", at = @At("HEAD"), cancellable = true)
-    private void onMouseClicked(Click click, boolean consumed, CallbackInfoReturnable<Boolean> cir) {
+    private void onMouseClicked(double mouseX, double mouseY, int button, CallbackInfoReturnable<Boolean> cir) {
         if (virtualKeyboard != null) {
-            if (virtualKeyboard.mouseClicked(click.mouseX(), click.mouseY(), click.button())) {
+            if (virtualKeyboard.mouseClicked(mouseX, mouseY, button)) {
                 cir.setReturnValue(true);
             }
         }
     }
-    
-    // FIXED: Override now uses Click record (1.21.11)
+
     @Override
-    public boolean mouseReleased(Click click) {
+    public boolean mouseReleased(double mouseX, double mouseY, int button) {
         if (virtualKeyboard != null) {
-            if (virtualKeyboard.mouseReleased(click.mouseX(), click.mouseY(), click.button())) {
+            if (virtualKeyboard.mouseReleased(mouseX, mouseY, button)) {
                 return true;
             }
         }
-        return super.mouseReleased(click);
+        return super.mouseReleased(mouseX, mouseY, button);
     }
-    
-    // FIXED: Override now uses Click record (1.21.11)
+
     @Override
-    public boolean mouseDragged(Click click, double deltaX, double deltaY) {
+    public boolean mouseDragged(double mouseX, double mouseY, int button, double deltaX, double deltaY) {
         if (virtualKeyboard != null) {
             ChatScreen screen = (ChatScreen) (Object) this;
-            if (virtualKeyboard.mouseDragged(click.mouseX(), click.mouseY(), click.button(), deltaX, deltaY, screen.width, screen.height)) {
+            if (virtualKeyboard.mouseDragged(mouseX, mouseY, button, deltaX, deltaY, screen.width, screen.height)) {
                 return true;
             }
         }
-        return super.mouseDragged(click, deltaX, deltaY);
+        return super.mouseDragged(mouseX, mouseY, button, deltaX, deltaY);
     }
 }
