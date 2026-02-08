bbfc5aee57b7861c9c091689636ac04635711193
BobScottz <167954955+Zack694@users.noreply.github.com>
2026-02-08 18:40:05 +0800

    Update SignEditScreenMixin.java

--- src/main/java/com/commandgui/mixin/SignEditScreenMixin.java
+++ src/main/java/com/commandgui/mixin/SignEditScreenMixin.java
@@ -4,6 +4,7 @@ import com.commandgui.VirtualKeyboard;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.ingame.AbstractSignEditScreen;
+import net.minecraft.client.gui.Click;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
