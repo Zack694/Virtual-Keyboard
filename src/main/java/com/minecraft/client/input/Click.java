0c19c1d61eaeec529a88b121489e2c41fca16c7e
BobScottz <167954955+Zack694@users.noreply.github.com>
2026-02-08 18:32:29 +0800

    Delete Click.java

--- src/main/java/com/minecraft/client/input/Click.java
+++
@@ -1,8 +0,0 @@
-package net.minecraft.client.input;
-
-/**
- * Temporary Click record for 1.21.11 compatibility
- * This should match Minecraft's Click record structure
- */
-public record Click(double mouseX, double mouseY, int button) {
-}
